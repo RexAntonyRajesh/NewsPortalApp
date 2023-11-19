e8ed7d4db3e2ef93f80a809d56789e96f29eb6e3
Antony Rajesh <Antony.Rajesh@thomsonreuters.com>
2023-11-20 00:25:54 +0530

    environment API service url automating changes

--- src/Services/article.service.ts
+++ src/Services/article.service.ts
@@ -1,13 +1,16 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { environment } from '../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ArticleService {
-apiUrl:string ="https://localhost:7281/api/Article"
-  constructor(private _http: HttpClient) {}
+apiUrl:string;
+  constructor(private _http: HttpClient) {
+    this.apiUrl = environment.newsPortalAPIUrl + 'Article'
+  }
 
   addArticle(data: any): Observable<any> {
     return this._http.post(this.apiUrl, data);
--- src/Services/category.service.ts
+++ src/Services/category.service.ts
@@ -1,15 +1,18 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { environment } from '../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CategoryService {
 
-  apiUrl:string ="https://localhost:7281/api/Category";
+  apiUrl:string;
   
-  constructor(private _http: HttpClient) {}
+  constructor(private _http: HttpClient) {
+    this.apiUrl = environment.newsPortalAPIUrl + 'Category';
+  }
 
   getCategoryList(): Observable<any> {
     return this._http.get(this.apiUrl);
